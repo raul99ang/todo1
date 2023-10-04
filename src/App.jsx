1ef4dcc3b76af51daa6f831ac6c2afcc2d91c18f
raul99ang <136626634+raul99ang@users.noreply.github.com>
2023-10-03 18:46:55 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1 className="bg-gradient-to-r from-indigo-400 to-pink-400 bg-clip-text text-8xl font-extrabold text-transparent">
+        We love tailwind 💙
+      </h1>
+      <p className="p-4 pl-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+      </p>
+    </div>
+  );
+}
+
+export default App;
